b6658adce9bf838f736e0287c884b07742ac9fce
Sebastián Flores <00196919@uca.edu.sv>
2023-08-23 21:08:22 -0600

    fix:

--- src/app/components/post-card.tsx
+++ src/app/components/post-card.tsx
@@ -1,60 +1,48 @@
 import { Card, CardHeader, CardBody, CardFooter, Avatar, Button } from '@nextui-org/react'
-import { useState } from 'react'
+import Link from 'next/link';
+
+import { IconHeart, IconMessageCircle, IconRepeat } from '@tabler/icons-react';
 
 export default function PostCard ({
-  userName,
   userFullName,
+  userName,
   avatarUrl,
   content
 }: {
-  userName: string
   userFullName: string
+  userName: string
   avatarUrl: string
   content: string
 }) {
-  const [isFollowed, setIsFollowed] = useState(false)
-
   return (
-    <Card className="max-w-[340px]">
+    <Card className="shadow-none bg-transparent hover:bg-slate-800 transition border-b rounded-none cursor-pointer border-white/20">
       <CardHeader className="justify-between">
-        <div className="flex gap-5">
-          <Avatar isBordered radius="full" size="md" src="/avatars/avatar-1.png" />
+        <div className="flex gap-x-2">
+          <Link href={`/${userName}`}>
+            <Avatar radius="full" size="md" src={avatarUrl} />
+          </Link>
           <div className="flex flex-col gap-1 items-start justify-center">
-            <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
-            <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
+            <h4 className="text-small font-semibold leading-none text-default-600">{userFullName}</h4>
+            <h5 className="text-small tracking-tight text-default-400">@{userName}</h5>
           </div>
         </div>
-        <Button
-          className={isFollowed ? 'bg-transparent text-foreground border-default-200' : ''}
-          color='primary'
-          radius='full'
-          size='sm'
-          variant={isFollowed ? 'bordered' : 'solid'}
-          onPress={() => setIsFollowed(!isFollowed)}
-        >
-          {isFollowed ? 'Unfollow' : 'Follow'}
-        </Button>
+
       </CardHeader>
-      <CardBody className='px-3 py-0 text-small text-default-400'>
+      <CardBody className="px-3 py-0 text-xs text-white bg-transparent">
         <p>
-          Frontend developer and UI/UX enthusiast. Join me on this coding adventure!
+          {content}
         </p>
-        <span className='pt-2'>
-          #FrontendWithZoey
-          <span className='py-2' aria-label='computer' role='img'>
-            💻
-          </span>
-        </span>
       </CardBody>
-      <CardFooter className='gap-3'>
-        <div className='flex gap-1'>
-          <p className='font-semibold text-default-400 text-small'>4</p>
-          <p className=' text-default-400 text-small'>Following</p>
-        </div>
-        <div className="flex gap-1">
-          <p className="font-semibold text-default-400 text-small">97.1K</p>
-          <p className="text-default-400 text-small">Followers</p>
-        </div>
+      <CardFooter className="gap-3">
+        <button>
+          <IconMessageCircle className='w-4 h-4' />
+        </button>
+        <button>
+          <IconHeart className='w-4 h-4' />
+        </button>
+        <button>
+          <IconRepeat className='w-4 h-4' />
+        </button>
       </CardFooter>
     </Card>
   )
