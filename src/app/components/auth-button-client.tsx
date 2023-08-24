2fedd6cf93259b1ed07b527a89916a8946a9b0de
Sebs14 <00196919@uca.edu.sv>
2023-08-24 01:05:36 -0600

    style: :lipstick: just added one line
    added one line

--- src/app/components/auth-button-client.tsx
+++ src/app/components/auth-button-client.tsx
@@ -8,6 +8,7 @@ import { Button } from '@nextui-org/button'
 export function AuthButton ({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient()
   const router = useRouter()
+
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
